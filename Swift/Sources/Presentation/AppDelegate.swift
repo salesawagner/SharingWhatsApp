ebd77eb3d5a00290a682e735cd175eff3ec13e9d
Wagner Sales <salesawagner@gmail.com>
2016-01-12 02:43:00 -0200

    Swift started

---
+++ Swift/Sources/Presentation/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SharingWhatsApp
+//
+//  Created by Wagner Sales on 1/12/16.
+//  Copyright © 2016 Wagner Sales. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
---
+++ Swift/Sources/Presentation/Interface/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SharingWhatsApp
+//
+//  Created by Wagner Sales on 1/12/16.
+//  Copyright © 2016 Wagner Sales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var imageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
